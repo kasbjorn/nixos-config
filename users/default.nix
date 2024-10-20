20d4c6796d8a8611b8596589d32e818f23d30bf2
gwfisher <praetor@sdf.org>
2024-10-20 14:45:46 -0500

    added docker

--- users/default.nix
+++ users/default.nix
@@ -4,7 +4,7 @@
     users.users.wfisher = {
     	isNormalUser = true;
     	description = "Wes Fisher";
-    	extraGroups = [ "networkmanager" "wheel" "podman" "libvirt"];
+    	extraGroups = [ "networkmanager" "wheel" "docker" "libvirt"];
 		shell = pkgs.zsh;
   	};
 }
