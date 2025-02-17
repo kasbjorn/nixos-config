a106e514d893a53eb560951551c05117d1d8da43
Knut Asbjornsen <praetor207@gmail.com>
2025-02-16 18:15:38 -0600

    audio stuff

--- users/default.nix
+++ users/default.nix
@@ -5,7 +5,7 @@
     	users.users.kasbjorn = {
     		isNormalUser = true;
     		description = "Knut Asbjorn";
-    		extraGroups = [ "networkmanager" "wheel" "docker" "libvirt" "lp" "scanner" config.services.kubo.group];
+    		extraGroups = [ "audio" "jackaudio" "networkmanager" "wheel" "docker" "libvirt" "lp" "scanner" config.services.kubo.group];
 			shell = pkgs.zsh;
   	};
 }
