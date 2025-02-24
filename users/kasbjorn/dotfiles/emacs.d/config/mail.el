fda7685035bc26cb141486373748a02236f54a2b
Knut Asbjornsen <praetor207@gmail.com>
2025-02-23 18:45:51 -0600

    mail stuff

--- users/kasbjorn/dotfiles/emacs.d/config/mail.el
+++ users/kasbjorn/dotfiles/emacs.d/config/mail.el
@@ -2,4 +2,8 @@
   :ensure nil
   :config
   (setq mu4e-change-filenames-when-moving t)
-  (setq mu4e-maildir "~/Mail"))
+  (setq mu4e-maildir "~/Mail")
+  (setq mu4e-get-mail-command "mbsync -a")
+  (setq user-full-name "kasbjornsen@frostbyte.works"
+      user-mail-address "kasbjornsen@frostbyte.works")
+  )
