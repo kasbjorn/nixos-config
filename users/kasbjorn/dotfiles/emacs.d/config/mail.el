8521d56fa6d7f36c2256a24034bb17add3398913
Knut Asbjornsen <praetor207@gmail.com>
2025-02-23 19:09:38 -0600

    mail stuff

--- users/kasbjorn/dotfiles/emacs.d/config/mail.el
+++ users/kasbjorn/dotfiles/emacs.d/config/mail.el
@@ -5,5 +5,6 @@
   (setq mu4e-maildir "~/Mail")
   (setq mu4e-get-mail-command "mbsync -a")
   (setq user-full-name "kasbjornsen@frostbyte.works"
-      user-mail-address "kasbjornsen@frostbyte.works")
-  )
+	user-mail-address "kasbjornsen@frostbyte.works")
+  :init
+  (mu4e t))
