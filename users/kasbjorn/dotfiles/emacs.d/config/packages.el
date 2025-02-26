78a8685e8c6337b0cc0290153e6b8476ca1ae3df
Knut Asbjornsen <praetor207@gmail.com>
2025-02-25 18:15:32 -0600

    stuffs

--- users/kasbjorn/dotfiles/emacs.d/config/packages.el
+++ users/kasbjorn/dotfiles/emacs.d/config/packages.el
@@ -69,10 +69,20 @@
   (global-set-key (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
 
-(use-package perspective
-  :bind
-  ("C-x C-b" . persp-ivy-switch-buffer)         ; or use a nicer switcher, see below
+(use-package tabspaces
+  ;; use this next line only if you also use straight, otherwise ignore it. 
+  :straight (:type git :host github :repo "mclear-tools/tabspaces")
+  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
   :custom
-  (persp-mode-prefix-key (kbd "C-z"))  ; pick your own prefix key here
-  :init
-  (persp-mode))
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  (tabspaces-initialize-project-with-todo t)
+  (tabspaces-todo-file-name "project-todo.org")
+  ;; sessions
+  (tabspaces-session t)
+  (tabspaces-session-auto-restore t)
+  (tab-bar-new-tab-choice "*scratch*"))
