737b7aec47858d4d09d7990edde39373be4becc8
Knut Asbjorjsen <kasbjornsen@frostbyte.works>
2025-03-25 16:34:00 -0500

    Added mail stuff

--- users/kasbjornsen/dotfiles/emacs.d/config/mail.el
+++ users/kasbjornsen/dotfiles/emacs.d/config/mail.el
@@ -2,15 +2,22 @@
   :ensure t
   :config
   (setq mu4e-change-filenames-when-moving t)
-  (setq mu4e-maildir "Mail")
+  (setq mu4e-maildir "~/Maildir")
   (setq mu4e-get-mail-command "mbsync -a")
   (setq user-full-name "Knut Asbjornsen"
 	user-mail-address "kasbjornsen@frostbyte.works")
 
-  (setq mu4e-drafts-folder "/Drafts")
-  (setq mu4e-sent-folder   "/Sent")
-  (setq mu4e-refile-folder "/All Mail")
-  (setq mu4e-trash-folder  "/Trash")
+  (setq mu4e-drafts-folder "/[frostbyte]/Drafts")
+  (setq mu4e-sent-folder   "/[frostbyte]/Sent Mail")
+  (setq mu4e-refile-folder "/[frostbyte]/All Mail")
+  (setq mu4e-trash-folder  "/[frostbyte]/Trash")
+
+  (setq mu4e-maildir-shortcuts
+    '((:maildir "/Inbox"    :key ?i)
+      (:maildir "/[frostbyte]/Sent Mail" :key ?s)
+      (:maildir "/[frostbyte]/Trash"     :key ?t)
+      (:maildir "/[frostbyte]/Drafts"    :key ?d)
+      (:maildir "/[frostbyte]/All Mail"  :key ?a))))
 
   (setq smtpmail-smtp-server "smtp.ionos.com"
       smtpmail-smtp-service 465
