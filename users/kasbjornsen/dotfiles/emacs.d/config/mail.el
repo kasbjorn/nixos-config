742f6b620955842e4017d7b66afbab274899cdb9
Knut Asbjorjsen <kasbjornsen@frostbyte.works>
2025-03-27 23:01:11 -0500

    mu4e config

--- users/kasbjornsen/dotfiles/emacs.d/config/mail.el
+++ users/kasbjornsen/dotfiles/emacs.d/config/mail.el
@@ -1,28 +1,38 @@
 (use-package mu4e
   :ensure t
   :config
+
   (setq mu4e-change-filenames-when-moving t)
-  (setq mu4e-maildir "~/Maildir")
+  (setq mu4e-update-interval (* 10 60))
   (setq mu4e-get-mail-command "mbsync -a")
-  (setq user-full-name "Knut Asbjornsen"
-	user-mail-address "kasbjornsen@frostbyte.works")
-
-  (setq mu4e-drafts-folder "/[frostbyte]/Drafts")
-  (setq mu4e-sent-folder   "/[frostbyte]/Sent Mail")
-  (setq mu4e-refile-folder "/[frostbyte]/All Mail")
-  (setq mu4e-trash-folder  "/[frostbyte]/Trash")
-
+  (setq mu4e-maildir "~/Maildir")
+  (setq mu4e-compose-context-policy 'ask-if-none)
+  (setq mu4e-compose-format-flowed t)  
+  
+  (setq mu4e-contexts
+	(list
+	 (make-mu4e-context
+	  :name "PraetorTEL"
+	  :match-func
+	  (lambda (msg)
+	    (when msg
+	      (string-prefix-p "/PraetorTEL" (mu4e-message-field msg :maildir))))
+	  :vars
+	  '((user-mail-address . "kasbjornsen@praetor.tel")
+	    (user-full-name . "Knute Asbjornsen")
+	    (smtpmail-smtp-server . "127.0.0.1")
+	    (smtpmail-smtp-server . 1025)
+	    (smtpmail-stream-type . 'starttls)
+	    (mu4e-drafts-folder . "/[PraetorTEL]/Drafts")
+	    (mu4e-sent-folder . "/[PraetorTEL]/Sent")
+	    (mu4e-refile-folder . "/[PraetorTEL/All Mail")
+	    (mu4e-trash-folder . "/[PraetorTEL/Trash")))))
+  
   (setq mu4e-maildir-shortcuts
     '((:maildir "/Inbox"    :key ?i)
-      (:maildir "/[frostbyte]/Sent Mail" :key ?s)
-      (:maildir "/[frostbyte]/Trash"     :key ?t)
-      (:maildir "/[frostbyte]/Drafts"    :key ?d)
-      (:maildir "/[frostbyte]/All Mail"  :key ?a))))
-
-  (setq smtpmail-smtp-server "smtp.ionos.com"
-      smtpmail-smtp-service 465
-      smtpmail-stream-type  'ssl)
-
-  (setq message-send-mail-function 'smtpmail-send-it)
+      (:maildir "/[PraetorTEL]/Sent Mail" :key ?s)
+      (:maildir "/[PraetorTEL]/Trash"     :key ?t)
+      (:maildir "/[PraetorTEL]/Drafts"    :key ?d)
+      (:maildir "/[PraetorTEL]/All Mail"  :key ?a)))
   :init
   (mu4e t))
