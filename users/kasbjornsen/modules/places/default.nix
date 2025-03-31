e4f3906b690c568ced2e3758d8ff4d6f9428c5a0
Knut Asbjorjsen <kasbjornsen@praetor.tel>
2025-03-30 20:44:44 -0500

    finishing touches

--- users/kasbjornsen/modules/places/default.nix
+++ users/kasbjornsen/modules/places/default.nix
@@ -21,8 +21,7 @@
       type = crypt
       remote = SDF-MA:/meta/p/praetor/Vault
       filename_encryption = standard
-      password = EcbyzHhWzw_eHAiulclgkf1w0otSQUVwr7vGKA
-  '';
+      password ='' {$sops.secrets.secret.vault_password};
 
   systemd.user.services.proton-mount = {
     Unit = {
