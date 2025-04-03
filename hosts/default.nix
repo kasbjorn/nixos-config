d640ee29319a07f46e47e1c8dc39121cbdca1719
Knut Asbjorjsen <kasbjornsen@praetor.tel>
2025-04-03 03:34:45 -0500

    more sops

---
+++ hosts/default.nix
@@ -0,0 +1,13 @@
+{ inputs, config, pkgs , ... }:
+
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+  
+  sops = {
+    defaultSopsFile = ./private.yml;
+    validateSopsFiles = false;
+    age.keyFile = /home/kasbjornsen/.config/sops/age/keys.txt;
+ };
+}
