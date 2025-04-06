1f48e27fd7fb588d162773de677b1a1df472a106
Knut Asbjorjsen <kasbjornsen@praetor.tel>
2025-04-05 21:01:10 -0500

    more sops

--- home/kasbjornsen/secrets/default.nix
+++ home/kasbjornsen/secrets/default.nix
@@ -1,9 +1,9 @@
 { inputs, config, pkgs, lib, ...}:
 {
   sops = {
-    age.keyFile = "/home/tom/.ssh/sops/age/keys.txt"; # must have no password!
+    age.keyFile = "/home/kasbjornsen/.config/sops/age/keys.txt"; # must have no password!
    
-    defaultSopsFile = ./secrets/git.yaml;
+    defaultSopsFile = ./secrets.yaml;
 
     defaultSymlinkPath = "/run/user/1000/secrets";
     defaultSecretsMountPoint = "/run/user/1000/secrets.d";
