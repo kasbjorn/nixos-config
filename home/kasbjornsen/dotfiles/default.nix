b7b318937e0dbfc79a193bd91f97c2989f1af472
Knut Asbjorjsen <kasbjornsen@praetor.tel>
2025-04-27 12:21:47 -0500

    feeds.org stuff

--- home/kasbjornsen/dotfiles/default.nix
+++ home/kasbjornsen/dotfiles/default.nix
@@ -7,12 +7,12 @@
             {
               source = config.lib.file.mkOutOfStoreSymlink ./emacs.d/init.el;
             };
-          ".emacs.d/feeds.org" = ./emacs.d/feeds.org;
           ".emacs.d/snippets" =
             {
               source = config.lib.file.mkOutOfStoreSymlink ./emacs.d/snippets;
             };
 	        ".emacs.d/config".source = ./emacs.d/config;
+          ".emacs.d/feeds.org".source = ./emacs.d/feeds.org;
           ".wallpaper".source = ./wallpaper;
           ".config/niri".source = ./niri;
           ".config/wpaperd".source = ./wpaperd;
